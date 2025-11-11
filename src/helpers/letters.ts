de7bfc8f9c0df3e7d4845ec8b79cd4fbe45da5be
Mauricio Gutierrez <mauricio.gutierrez.dev@gmail.com>
2025-11-11 11:46:56 -0500

    feat: add available letters array for hangman gameplay

---
+++ src/helpers/letters.ts
@@ -0,0 +1,6 @@
+// Letras disponibles que el jugador puede elegir
+export const letters: string[] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
